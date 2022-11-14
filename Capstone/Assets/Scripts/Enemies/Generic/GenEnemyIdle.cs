7fdcba4b537bd815d6d386cf7884c46e1ca2268f
Tobie Allen <tallen@student.neumont.edu>
2022-11-14 15:22:48 -0700

    fixed enemy

--- Capstone/Assets/Scripts/Enemies/Generic/GenEnemyIdle.cs
+++ Capstone/Assets/Scripts/Enemies/Generic/GenEnemyIdle.cs
@@ -12,23 +12,21 @@ public class GenEnemyIdle : Node
 
     public GenEnemyIdle(Transform transform) { this.transform = transform; }
 
-    void Start()
-    {
-        initialPosition = this.transform.position; 
-    }
-
     public override NodeState Evaluate()
     {
         // every 3-5 seconds, do an idle animation 
-        Debug.Log("Enemy entered Idle"); 
+        Debug.Log("Enemy entered Idle");
+
+        if (initialPosition == new Vector3(0, 0, 0)) initialPosition = transform.position;
 
         if (transform.position != initialPosition)
         {
+            Debug.Log("Enemy moving towards Initial Pos"); 
             transform.position = Vector3.MoveTowards(transform.position, initialPosition, GenEnemyBT.speed * Time.deltaTime);
-            transform.LookAt(initialPosition); 
+            transform.LookAt(initialPosition);            
         }
 
-        transform.rotation = quaternion;
+        //transform.rotation = quaternion;
 
         state = NodeState.RUNNING;
         return state; 
